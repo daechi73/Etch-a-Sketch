ea296a9286c3bc3f48f13d6f4daf20058ea3274f
J <59581793+daechi73@users.noreply.github.com>
2023-01-18 23:35:11 -0500

    added js.js:added divMenu that holds range input and a div that shows its selected value, showRangeValue created

--- js.js
+++ js.js
@@ -10,3 +10,26 @@ divContainer.style.flexBasis = "auto";
 document.body.appendChild(divContainer);
 document.body.style.backgroundColor = "black";
 document.body.style.display = "flex";
+
+let divMenu = document.createElement("div");
+let inputRange = document.createElement("input");
+let rangeValue = document.createElement("div");
+rangeValue.id = "rangeValue";
+rangeValue.style.color = "white";
+rangeValue.textContent = "value:";
+inputRange.type = "range";
+inputRange.classList.add = "inputRange";
+inputRange.min = "1";
+inputRange.max = "60";
+inputRange.value = "16";
+
+divMenu.appendChild(inputRange);
+divMenu.appendChild(rangeValue);
+document.body.insertBefore(divMenu, divContainer);
+function showRangeValue(val) {
+  rangeValue.textContent = `${val} x ${val}`;
+}
+inputRange.addEventListener("change", (e) => {
+  setGrid(e.target.value);
+  showRangeValue(e.target.value);
+});
