a2efcc73c5afd88eef651a2a79db8d0cf413bc5a
J <59581793+daechi73@users.noreply.github.com>
2023-01-18 23:32:18 -0500

    added -js.js:added divContainer that hold all the squre divs

---
+++ js.js
@@ -0,0 +1,12 @@
+let divContainer = document.createElement("div");
+divContainer.classList.add("divContainer");
+divContainer.id = "divContainer";
+divContainer.style.border = "solid";
+divContainer.style.borderColor = "white";
+divContainer.style.display = "flex";
+divContainer.style.flexDirection = "column";
+divContainer.style.flexBasis = "auto";
+
+document.body.appendChild(divContainer);
+document.body.style.backgroundColor = "black";
+document.body.style.display = "flex";
